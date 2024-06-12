ccccd9d0cf901d469c332ea196f456159c3e392e
hueindahaus <alexanderhuangen@live.se>
2024-06-12 21:48:29 +0200

    added tsserver

--- nvim/setup.bash
+++ nvim/setup.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Install nvim
+read -p "Do you want to install all necessary binaries (y/n)? " -n 1 -r
+echo # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
 sudo snap install --beta nvim --classic
 sudo apt install clang
 sudo apt install zip
@@ -8,9 +12,11 @@ sudo apt install python3
 sudo apt install python3-pip
 sudo apt install python3.10-venv
 sudo apt install ripgrep
+fi
+
 
 # install devpod for devcontainers
-curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
+# curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
 
 # Copy over configs
 config_path="${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
--- nvim/src/init.lua
+++ nvim/src/init.lua
@@ -623,17 +623,17 @@ require("lazy").setup({
 						},
 					},
 				},
-				-- isort = {version="5.11.4"},
-				-- black = {version="24.2.0"},
+				isort = { version = "5.11.4" },
+				-- black = { version = "24.2.0" },
 				-- ruff = {},
-				-- rust_analyzer = {},
+				rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
+				tsserver = {},
 				--
 
 				lua_ls = {
