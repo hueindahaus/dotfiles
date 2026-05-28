3cce0b831226d8fdb3f3460d64acd77e737bbf08
Alexander Huang <alexander.huang@zenseact.com>
2026-05-28 09:23:10 +0200

    ty fix

--- nvim/.config/nvim/lsp/ty.lua
+++ nvim/.config/nvim/lsp/ty.lua
@@ -4,6 +4,7 @@ return {
   root_dir = vim.fs.root(0, { ".git/", "pyproject.toml" }),
   settings = {
     ty = {
+      diagnosticMode = 'workspace',
       completions = {
         autoImport = true,
       },
